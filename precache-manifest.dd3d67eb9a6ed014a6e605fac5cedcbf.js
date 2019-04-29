c3a16a4dfcf38904c58db324cecc43b8d50afeeb
Han Moon <hanmoon@Hans-MacBook-Pro.local>
2019-04-28 21:27:09 -0600

    Updates

---
+++ precache-manifest.dd3d67eb9a6ed014a6e605fac5cedcbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bc9005db4a184849e277f49bd29749",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "bedcaa9ebe2469c1b912",
+    "url": "/clickyGame/static/css/main.122f71f4.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "bedcaa9ebe2469c1b912",
+    "url": "/clickyGame/static/js/main.54e50dc3.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
